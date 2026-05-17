ae1502f5334bee1cfb72482b4bf8fc5f2974cc6c
ning.huang <ning.huang.ridson@gmail.com>
2026-05-17 16:42:32 +0800

    init repo

---
+++ 00_pytorch_fundamentals/main.py
@@ -0,0 +1,17 @@
+import torch
+
+# Scalar
+scalar = torch.tensor(7)
+scalar
+
+
+# Vector
+vector = torch.tensor([7, 7])
+vector
+
+
+# Tensor
+TENSOR = torch.tensor([[[1, 2, 3],
+                        [3, 6, 9],
+                        [2, 4, 5]]])
+TENSOR
---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from pytorch-deep-learning-code!")
+
+
+if __name__ == "__main__":
+    main()
