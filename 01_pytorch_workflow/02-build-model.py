f12e3df5875a0b51e3dc97c12a9039da2ef0f263
ning.huang <ning.huang.ridson@gmail.com>
2026-05-18 21:30:31 +0800

    Matplotlib 弹出窗口显示图像

--- 01_pytorch_workflow/02-build-model.py
+++ 01_pytorch_workflow/02-build-model.py
@@ -1,7 +1,55 @@
 import torch
-from torch import nn
+from torch import nn # nn contains all of PyTorch's building blocks for neural networks
+import matplotlib.pyplot as plt
 
-from 01_data_prepare import X_train, y_train, X_test, y_test, plot_predictions
+# Create *known* parameters
+weight = 0.7
+bias = 0.3
+
+# Create data
+start = 0
+end = 1
+step = 0.02
+X = torch.arange(start, end, step).unsqueeze(dim=1)
+y = weight * X + bias
+
+X[:10], y[:10]
+
+
+# Split data into training and test sets
+# Create train/test split
+train_split = int(0.8 * len(X)) # 80% of data used for training set, 20% for testing 
+X_train, y_train = X[:train_split], y[:train_split]
+X_test, y_test = X[train_split:], y[train_split:]
+
+len(X_train), len(y_train), len(X_test), len(y_test)
+
+
+def plot_predictions(train_data=X_train, 
+                     train_labels=y_train, 
+                     test_data=X_test, 
+                     test_labels=y_test, 
+                     predictions=None):
+  """
+  Plots training data, test data and compares predictions.
+  """
+  plt.figure(figsize=(10, 7))
+
+  # Plot training data in blue
+  plt.scatter(train_data, train_labels, c="b", s=4, label="Training data")
+  
+  # Plot test data in green
+  plt.scatter(test_data, test_labels, c="g", s=4, label="Testing data")
+
+  if predictions is not None:
+    # Plot the predictions in red (predictions were made on the test data)
+    plt.scatter(test_data, predictions, c="r", s=4, label="Predictions")
+
+  # Show the legend
+  plt.legend(prop={"size": 14});
+
+
+
 
 # Create a Linear Regression model class
 class LinearRegressionModel(nn.Module): # <- almost everything in PyTorch is a nn.Module (think of this as neural network lego blocks)
@@ -43,3 +91,7 @@ with torch.inference_mode():
 #   y_preds = model_0(X_test)
 
 plot_predictions(predictions=y_preds)
+
+
+# 👇 加上这一行，告诉 Matplotlib 弹出窗口显示图像
+plt.show()
