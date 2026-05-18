ff7b498fe569baeba7dbc6a4668f4b968ee5a0b0
ning.huang <ning.huang.ridson@gmail.com>
2026-05-18 17:30:43 +0800

    04_pytorch_custom_datasets

---
+++ 04_pytorch_custom_datasets/01-prepare-data.py
@@ -0,0 +1,97 @@
+import requests
+import zipfile
+from pathlib import Path
+
+# Setup path to data folder
+data_path = Path("data/")
+image_path = data_path / "pizza_steak_sushi"
+
+# If the image folder doesn't exist, download it and prepare it... 
+if image_path.is_dir():
+    print(f"{image_path} directory exists.")
+else:
+    print(f"Did not find {image_path} directory, creating one...")
+    image_path.mkdir(parents=True, exist_ok=True)
+    
+    # Download pizza, steak, sushi data
+    with open(data_path / "pizza_steak_sushi.zip", "wb") as f:
+        request = requests.get("https://github.com/mrdbourke/pytorch-deep-learning/raw/main/data/pizza_steak_sushi.zip")
+        print("Downloading pizza, steak, sushi data...")
+        f.write(request.content)
+
+    # Unzip pizza, steak, sushi data
+    with zipfile.ZipFile(data_path / "pizza_steak_sushi.zip", "r") as zip_ref:
+        print("Unzipping pizza, steak, sushi data...") 
+        zip_ref.extractall(image_path)
+
+
+
+import os
+def walk_through_dir(dir_path):
+  """
+  Walks through dir_path returning its contents.
+  Args:
+    dir_path (str or pathlib.Path): target directory
+  
+  Returns:
+    A print out of:
+      number of subdiretories in dir_path
+      number of images (files) in each subdirectory
+      name of each subdirectory
+  """
+  for dirpath, dirnames, filenames in os.walk(dir_path):
+    print(f"There are {len(dirnames)} directories and {len(filenames)} images in '{dirpath}'.")
+
+
+# Setup train and testing paths
+train_dir = image_path / "train"
+test_dir = image_path / "test"
+
+train_dir, test_dir
+
+
+
+
+import random
+from PIL import Image
+
+# Set seed
+random.seed(42) # <- try changing this and see what happens
+
+# 1. Get all image paths (* means "any combination")
+image_path_list = list(image_path.glob("*/*/*.jpg"))
+
+# 2. Get random image path
+random_image_path = random.choice(image_path_list)
+
+# 3. Get image class from path name (the image class is the name of the directory where the image is stored)
+image_class = random_image_path.parent.stem
+
+# 4. Open image
+img = Image.open(random_image_path)
+
+# 5. Print metadata
+print(f"Random image path: {random_image_path}")
+print(f"Image class: {image_class}")
+print(f"Image height: {img.height}") 
+print(f"Image width: {img.width}")
+img
+
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Turn the image into an array
+img_as_array = np.asarray(img)
+
+# Plot the image with matplotlib
+plt.figure(figsize=(10, 7))
+plt.imshow(img_as_array)
+plt.title(f"Image class: {image_class} | Image shape: {img_as_array.shape} -> [height, width, color_channels]")
+plt.axis(False);
+
+
+
+
+
---
+++ 04_pytorch_custom_datasets/03-custom-dataset.py
@@ -0,0 +1,71 @@
+# Write a custom dataset class (inherits from torch.utils.data.Dataset)
+from torch.utils.data import Dataset
+
+# 1. Subclass torch.utils.data.Dataset
+class ImageFolderCustom(Dataset):
+    
+    # 2. Initialize with a targ_dir and transform (optional) parameter
+    def __init__(self, targ_dir: str, transform=None) -> None:
+        
+        # 3. Create class attributes
+        # Get all image paths
+        self.paths = list(pathlib.Path(targ_dir).glob("*/*.jpg")) # note: you'd have to update this if you've got .png's or .jpeg's
+        # Setup transforms
+        self.transform = transform
+        # Create classes and class_to_idx attributes
+        self.classes, self.class_to_idx = find_classes(targ_dir)
+
+    # 4. Make function to load images
+    def load_image(self, index: int) -> Image.Image:
+        "Opens an image via a path and returns it."
+        image_path = self.paths[index]
+        return Image.open(image_path) 
+    
+    # 5. Overwrite the __len__() method (optional but recommended for subclasses of torch.utils.data.Dataset)
+    def __len__(self) -> int:
+        "Returns the total number of samples."
+        return len(self.paths)
+    
+    # 6. Overwrite the __getitem__() method (required for subclasses of torch.utils.data.Dataset)
+    def __getitem__(self, index: int) -> Tuple[torch.Tensor, int]:
+        "Returns one sample of data, data and label (X, y)."
+        img = self.load_image(index)
+        class_name  = self.paths[index].parent.name # expects path in data_folder/class_name/image.jpeg
+        class_idx = self.class_to_idx[class_name]
+
+        # Transform if necessary
+        if self.transform:
+            return self.transform(img), class_idx # return data, label (X, y)
+        else:
+            return img, class_idx # return data, label (X, y)
+
+
+
+# Augment train data
+train_transforms = transforms.Compose([
+    transforms.Resize((64, 64)),
+    transforms.RandomHorizontalFlip(p=0.5),
+    transforms.ToTensor()
+])
+
+# Don't augment test data, only reshape
+test_transforms = transforms.Compose([
+    transforms.Resize((64, 64)),
+    transforms.ToTensor()
+])
+
+
+train_data_custom = ImageFolderCustom(targ_dir=train_dir, 
+                                      transform=train_transforms)
+test_data_custom = ImageFolderCustom(targ_dir=test_dir, 
+                                     transform=test_transforms)
+train_data_custom, test_data_custom
+
+
+# Check for equality amongst our custom Dataset and ImageFolder Dataset
+print((len(train_data_custom) == len(train_data)) & (len(test_data_custom) == len(test_data)))
+print(train_data_custom.classes == train_data.classes)
+print(train_data_custom.class_to_idx == train_data.class_to_idx)
+
+
+
