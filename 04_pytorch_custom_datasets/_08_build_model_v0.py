ea0385e6a1c3a27fe2d1a1dba4cc3ca3ef6b4697
ning.huang <ning.huang.ridson@gmail.com>
2026-05-19 11:27:35 +0800

    build model

--- 04_pytorch_custom_datasets/_08_build_model_v0.py
+++ 04_pytorch_custom_datasets/_08_build_model_v0.py
@@ -46,3 +46,89 @@ test_dataloader_simple = DataLoader(test_data_simple,
                                     num_workers=NUM_WORKERS)
 
 print(train_dataloader_simple, test_dataloader_simple)
+
+
+
+
+
+import torch
+from torch import nn 
+
+class TinyVGG(nn.Module):
+    """
+    Model architecture copying TinyVGG from: 
+    https://poloclub.github.io/cnn-explainer/
+    """
+    def __init__(self, input_shape: int, hidden_units: int, output_shape: int) -> None:
+        super().__init__()
+        self.conv_block_1 = nn.Sequential(
+            nn.Conv2d(in_channels=input_shape, 
+                      out_channels=hidden_units, 
+                      kernel_size=3, # how big is the square that's going over the image?
+                      stride=1, # default
+                      padding=1), # options = "valid" (no padding) or "same" (output has same shape as input) or int for specific number 
+            nn.ReLU(),
+            nn.Conv2d(in_channels=hidden_units, 
+                      out_channels=hidden_units,
+                      kernel_size=3,
+                      stride=1,
+                      padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2,
+                         stride=2) # default stride value is same as kernel_size
+        )
+        self.conv_block_2 = nn.Sequential(
+            nn.Conv2d(hidden_units, hidden_units, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(hidden_units, hidden_units, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(2)
+        )
+        self.classifier = nn.Sequential(
+            nn.Flatten(),
+            # Where did this in_features shape come from? 
+            # It's because each layer of our network compresses and changes the shape of our input data.
+            nn.Linear(in_features=hidden_units*16*16,
+                      out_features=output_shape)
+        )
+    
+    def forward(self, x: torch.Tensor):
+        x = self.conv_block_1(x)
+        # print(x.shape)
+        x = self.conv_block_2(x)
+        # print(x.shape)
+        x = self.classifier(x)
+        # print(x.shape)
+        return x
+        # return self.classifier(self.conv_block_2(self.conv_block_1(x))) # <- leverage the benefits of operator fusion
+
+
+
+# 2. 将所有执行逻辑放入 main 块中
+if __name__ == '__main__':
+
+    
+    device = "cpu"
+    torch.manual_seed(42)
+    model_0 = TinyVGG(input_shape=3, # number of color channels (3 for RGB) 
+                    hidden_units=10, 
+                    output_shape=len(train_data_simple.classes)).to(device)
+    print(model_0)
+
+    # 1. Get a batch of images and labels from the DataLoader
+    img_batch, label_batch = next(iter(train_dataloader_simple))
+
+    # 2. Get a single image from the batch and unsqueeze the image so its shape fits the model
+    img_single, label_single = img_batch[0].unsqueeze(dim=0), label_batch[0]
+    print(f"Single image shape: {img_single.shape}\n")
+
+    # 3. Perform a forward pass on a single image
+    model_0.eval()
+    with torch.inference_mode():
+        pred = model_0(img_single.to(device))
+        
+    # 4. Print out what's happening and convert model logits -> pred probs -> pred label
+    print(f"Output logits:\n{pred}\n")
+    print(f"Output prediction probabilities:\n{torch.softmax(pred, dim=1)}\n")
+    print(f"Output prediction label:\n{torch.argmax(torch.softmax(pred, dim=1), dim=1)}\n")
+    print(f"Actual label:\n{label_single}")
