012a2d42ff1c19c11570f82ff5cc39734f2481d7
SyPink6 <peng_sy@qq.com>
2026-05-22 14:37:17 +0800

    update

--- 02_pytorch_classification/04-non-linearity.py
+++ 02_pytorch_classification/04-non-linearity.py
@@ -1,38 +1,26 @@
-# Make and plot data
-import matplotlib.pyplot as plt
-from sklearn.datasets import make_circles
-
-n_samples = 1000
-
-X, y = make_circles(n_samples=1000,
-    noise=0.03,
-    random_state=42,
-)
-
-plt.scatter(X[:, 0], X[:, 1], c=y, cmap=plt.cm.RdBu)
-
-
-# Convert to tensors and split into train and test sets
+# Standard PyTorch imports
 import torch
-from sklearn.model_selection import train_test_split
-
-# Turn data into tensors
-X = torch.from_numpy(X).type(torch.float)
-y = torch.from_numpy(y).type(torch.float)
-
-# Split into train and test sets
-X_train, X_test, y_train, y_test = train_test_split(X, 
-                                                    y, 
-                                                    test_size=0.2,
-                                                    random_state=42
-)
-
-X_train[:5], y_train[:5]
-
+from torch import nn
+from data_setup import X_train, X_test, y_train, y_test
+# Import matplotlib for visualization
+import matplotlib.pyplot as plt
+from helper_functions import plot_predictions, plot_decision_boundary
+from sklearn.datasets import make_circles
 
 
 # Build model with non-linear activation function
 from torch import nn
+
+# Make device agnostic code
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+# # Calculate accuracy (a classification metric)
+def accuracy_fn(y_true, y_pred):
+    correct = torch.eq(y_true, y_pred).sum().item() # torch.eq() calculates where two tensors are equal
+    acc = (correct / len(y_pred)) * 100 
+    return acc
+
+
 class CircleModelV2(nn.Module):
     def __init__(self):
         super().__init__()
@@ -48,70 +36,105 @@ class CircleModelV2(nn.Module):
       # Intersperse the ReLU activation function between layers
        return self.layer_3(self.relu(self.layer_2(self.relu(self.layer_1(x)))))
 
-model_3 = CircleModelV2().to(device)
-print(model_3)
+def train_loop():
+
+    model_3 = CircleModelV2().to(device)
+    print(model_3)
+
+    # Setup loss and optimizer 
+    loss_fn = nn.BCEWithLogitsLoss()
+    optimizer = torch.optim.SGD(model_3.parameters(), lr=0.1)
 
 
+    # Fit the model
+    torch.manual_seed(42)
+    epochs = 1000
 
-# Setup loss and optimizer 
-loss_fn = nn.BCEWithLogitsLoss()
-optimizer = torch.optim.SGD(model_3.parameters(), lr=0.1)
+    for epoch in range(epochs):
+        # 1. Forward pass
+        y_logits = model_3(X_train).squeeze()
+        y_pred = torch.round(torch.sigmoid(y_logits)) # logits -> prediction probabilities -> prediction labels
+        
+        # 2. Calculate loss and accuracy
+        loss = loss_fn(y_logits, y_train) # BCEWithLogitsLoss calculates loss using logits
+        acc = accuracy_fn(y_true=y_train, 
+                        y_pred=y_pred)
+        
+        # 3. Optimizer zero grad
+        optimizer.zero_grad()
 
+        # 4. Loss backward
+        loss.backward()
 
-# Fit the model
-torch.manual_seed(42)
-epochs = 1000
+        # 5. Optimizer step
+        optimizer.step()
 
-# Put all data on target device
-X_train, y_train = X_train.to(device), y_train.to(device)
-X_test, y_test = X_test.to(device), y_test.to(device)
+        ### Testing
+        model_3.eval()
+        with torch.inference_mode():
+            # 1. Forward pass
+            test_logits = model_3(X_test).squeeze()
+            test_pred = torch.round(torch.sigmoid(test_logits)) # logits -> prediction probabilities -> prediction labels
+            # 2. Calculate loss and accuracy
+            test_loss = loss_fn(test_logits, y_test)
+            test_acc = accuracy_fn(y_true=y_test,
+                                    y_pred=test_pred)
 
-for epoch in range(epochs):
-    # 1. Forward pass
-    y_logits = model_3(X_train).squeeze()
-    y_pred = torch.round(torch.sigmoid(y_logits)) # logits -> prediction probabilities -> prediction labels
-    
-    # 2. Calculate loss and accuracy
-    loss = loss_fn(y_logits, y_train) # BCEWithLogitsLoss calculates loss using logits
-    acc = accuracy_fn(y_true=y_train, 
-                      y_pred=y_pred)
-    
-    # 3. Optimizer zero grad
-    optimizer.zero_grad()
+            # Print out what's happening
+            if epoch % 100 == 0:
+                print(f"Epoch: {epoch} | Loss: {loss:.5f}, Accuracy: {acc:.2f}% | Test Loss: {test_loss:.5f}, Test Accuracy: {test_acc:.2f}%")
 
-    # 4. Loss backward
-    loss.backward()
+    return model_3
 
-    # 5. Optimizer step
-    optimizer.step()
-
-    ### Testing
+def make_predict(model_3):
+    # Make predictions
     model_3.eval()
     with torch.inference_mode():
-      # 1. Forward pass
-      test_logits = model_3(X_test).squeeze()
-      test_pred = torch.round(torch.sigmoid(test_logits)) # logits -> prediction probabilities -> prediction labels
-      # 2. Calculate loss and accuracy
-      test_loss = loss_fn(test_logits, y_test)
-      test_acc = accuracy_fn(y_true=y_test,
-                             y_pred=test_pred)
+        y_preds = torch.round(torch.sigmoid(model_3(X_test))).squeeze()
+    print(y_preds[:10])
 
-    # Print out what's happening
-    if epoch % 100 == 0:
-        print(f"Epoch: {epoch} | Loss: {loss:.5f}, Accuracy: {acc:.2f}% | Test Loss: {test_loss:.5f}, Test Accuracy: {test_acc:.2f}%")
+def plot_predict(model_3):
+    plt.subplot(1, 2, 2)
+    plt.title("Test")
+    plot_decision_boundary(model_3, X_test, y_test) # model_3 = has non-linearity
+    plt.savefig(f"plot.png", dpi=300, bbox_inches='tight')
+    plt.close()  # 释放内存
 
 
-# Make predictions
-model_3.eval()
-with torch.inference_mode():
-    y_preds = torch.round(torch.sigmoid(model_3(X_test))).squeeze()
-y_preds[:10], y[:10] # want preds in same format as truth labels
+if __name__ == "__main__":
+    model_3 = train_loop()
+    make_predict(model_3)
+    plot_predict(model_3)
 
 
 
 
 
+def make_samples():
+    n_samples = 1000
+
+    X, y = make_circles(n_samples=1000,
+        noise=0.03,
+        random_state=42,
+    )
+
+    plt.scatter(X[:, 0], X[:, 1], c=y, cmap=plt.cm.RdBu)
 
 
+    # Convert to tensors and split into train and test sets
+    import torch
+    from sklearn.model_selection import train_test_split
 
+    # Turn data into tensors
+    X = torch.from_numpy(X).type(torch.float)
+    y = torch.from_numpy(y).type(torch.float)
+
+    # Split into train and test sets
+    X_train, X_test, y_train, y_test = train_test_split(X, 
+                                                        y, 
+                                                        test_size=0.2,
+                                                        random_state=42
+    )
+
+    X_train[:5], y_train[:5]
 
--- 02_pytorch_classification/05-ReLU-by-hand.py
+++ 02_pytorch_classification/05-ReLU-by-hand.py
@@ -1,29 +1,44 @@
-# Create a toy tensor (similar to the data going into our model(s))
-A = torch.arange(-10, 10, 1, dtype=torch.float32)
-A
 
-# Visualize the toy tensor
-plt.plot(A);
+# Standard PyTorch imports
+import torch
+from torch import nn
+from data_setup import X_train, X_test, y_train, y_test
+# Import matplotlib for visualization
+import matplotlib.pyplot as plt
+from helper_functions import plot_predictions, plot_decision_boundary
+from sklearn.datasets import make_circles
 
 
 # Create ReLU function by hand 
 def relu(x):
   return torch.maximum(torch.tensor(0), x) # inputs must be tensors
 
-# Pass toy tensor through ReLU function
-relu(A)
-
-# Plot ReLU activated toy tensor
-plt.plot(relu(A))
 
 
 # Create a custom sigmoid function
 def sigmoid(x):
   return 1 / (1 + torch.exp(-x))
 
-# Test custom sigmoid on toy tensor
-sigmoid(A)
+
+def main():
+
+  # Create a toy tensor (similar to the data going into our model(s))
+  A = torch.arange(-10, 10, 1, dtype=torch.float32)
+
+  # Visualize the toy tensor
+  plt.plot(A);
 
 
-# Plot sigmoid activated toy tensor
-plt.plot(sigmoid(A));
+  # Pass toy tensor through ReLU function
+  relu(A)
+
+  # Plot ReLU activated toy tensor
+  plt.plot(relu(A))
+
+
+  # Test custom sigmoid on toy tensor
+  sigmoid(A)
+
+
+  # Plot sigmoid activated toy tensor
+  plt.plot(sigmoid(A));
