15fa62fa8e9df2f0132ab51ca289d576b8d029ce
SyPink6 <peng_sy@qq.com>
2026-05-26 10:10:50 +0800

    train vit model

--- 08_pytorch_paper_replicating/test_create_embs.py
+++ 08_pytorch_paper_replicating/test_create_embs.py
@@ -6,7 +6,14 @@ import matplotlib.pyplot as plt
 import data_setup, engine, utils
 from helper_functions import set_seeds
 from patch_embedding import PatchEmbedding
+from msa import MultiheadSelfAttentionBlock
+from mlp import MLPBlock
+from encoder import TransformerEncoderBlock
+from vit_model import ViT
+from torchinfo import summary
 
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
 
 # Setup directories
 train_dir = "data/pizza_steak_sushi/train"
@@ -135,3 +142,238 @@ patchify = PatchEmbedding(in_channels=3,
 print(f"Input image shape: {image.unsqueeze(0).shape}")
 patch_embedded_image = patchify(image.unsqueeze(0)) # add an extra batch dimension on the 0th index, otherwise will error
 print(f"Output patch embedding shape: {patch_embedded_image.shape}")
+
+
+
+# Get the batch size and embedding dimension
+batch_size = patch_embedded_image.shape[0]
+embedding_dimension = patch_embedded_image.shape[-1]
+
+# Create the class token embedding as a learnable parameter that shares the same size as the embedding dimension (D)
+class_token = nn.Parameter(torch.ones(batch_size, 1, embedding_dimension), # [batch_size, number_of_tokens, embedding_dimension]
+                           requires_grad=True) # make sure the embedding is learnable
+
+# Show the first 10 examples of the class_token
+print(class_token[:, :, :10])
+
+# Print the class_token shape
+print(f"Class token shape: {class_token.shape} -> [batch_size, number_of_tokens, embedding_dimension]")
+
+# Add the class token embedding to the front of the patch embedding
+patch_embedded_image_with_class_embedding = torch.cat((class_token, patch_embedded_image),
+                                                      dim=1) # concat on first dimension
+
+# Print the sequence of patch embeddings with the prepended class token embedding
+print(patch_embedded_image_with_class_embedding)
+print(f"Sequence of patch embeddings with class token prepended shape: {patch_embedded_image_with_class_embedding.shape} -> [batch_size, number_of_patches, embedding_dimension]")
+
+
+
+height = 224 # H ("The training resolution is 224.")
+width = 224 # W
+
+# Calculate N (number of patches)
+number_of_patches = int((height * width) / patch_size**2)
+
+# Get embedding dimension
+embedding_dimension = patch_embedded_image_with_class_embedding.shape[2]
+
+# Create the learnable 1D position embedding
+position_embedding = nn.Parameter(torch.ones(1,
+                                             number_of_patches+1,
+                                             embedding_dimension),
+                                  requires_grad=True) # make sure it's learnable
+
+# Show the first 10 sequences and 10 position embedding values and check the shape of the position embedding
+print(position_embedding[:, :10, :10])
+print(f"Position embedding shape: {position_embedding.shape} -> [batch_size, number_of_patches, embedding_dimension]")
+
+
+
+# Add the position embedding to the patch and class token embedding
+patch_and_position_embedding = patch_embedded_image_with_class_embedding + position_embedding
+print(patch_and_position_embedding)
+print(f"Patch embeddings, class token prepended and positional embeddings added shape: {patch_and_position_embedding.shape} -> [batch_size, number_of_patches, embedding_dimension]")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+set_seeds()
+
+# 1. Set patch size
+patch_size = 16
+
+# 2. Print shape of original image tensor and get the image dimensions
+print(f"Image tensor shape: {image.shape}")
+height, width = image.shape[1], image.shape[2]
+
+# 3. Get image tensor and add batch dimension
+x = image.unsqueeze(0)
+print(f"Input image with batch dimension shape: {x.shape}")
+
+# 4. Create patch embedding layer
+patch_embedding_layer = PatchEmbedding(in_channels=3,
+                                       patch_size=patch_size,
+                                       embedding_dim=768)
+
+# 5. Pass image through patch embedding layer
+patch_embedding = patch_embedding_layer(x)
+print(f"Patching embedding shape: {patch_embedding.shape}")
+
+# 6. Create class token embedding
+batch_size = patch_embedding.shape[0]
+embedding_dimension = patch_embedding.shape[-1]
+class_token = nn.Parameter(torch.ones(batch_size, 1, embedding_dimension),
+                           requires_grad=True) # make sure it's learnable
+print(f"Class token embedding shape: {class_token.shape}")
+
+# 7. Prepend class token embedding to patch embedding
+patch_embedding_class_token = torch.cat((class_token, patch_embedding), dim=1)
+print(f"Patch embedding with class token shape: {patch_embedding_class_token.shape}")
+
+# 8. Create position embedding
+number_of_patches = int((height * width) / patch_size**2)
+position_embedding = nn.Parameter(torch.ones(1, number_of_patches+1, embedding_dimension),
+                                  requires_grad=True) # make sure it's learnable
+
+# 9. Add position embedding to patch embedding with class token
+patch_and_position_embedding = patch_embedding_class_token + position_embedding
+print(f"Patch and position embedding shape: {patch_and_position_embedding.shape}")
+
+
+
+
+
+
+
+# Create an instance of MSABlock
+multihead_self_attention_block = MultiheadSelfAttentionBlock(embedding_dim=768, # from Table 1
+                                                             num_heads=12) # from Table 1
+
+# Pass patch and position image embedding through MSABlock
+patched_image_through_msa_block = multihead_self_attention_block(patch_and_position_embedding)
+print(f"Input shape of MSA block: {patch_and_position_embedding.shape}")
+print(f"Output shape MSA block: {patched_image_through_msa_block.shape}")
+
+
+
+
+
+# Create an instance of MLPBlock
+mlp_block = MLPBlock(embedding_dim=768, # from Table 1
+                     mlp_size=3072, # from Table 1
+                     dropout=0.1) # from Table 3
+
+# Pass output of MSABlock through MLPBlock
+patched_image_through_mlp_block = mlp_block(patched_image_through_msa_block)
+print(f"Input shape of MLP block: {patched_image_through_msa_block.shape}")
+print(f"Output shape MLP block: {patched_image_through_mlp_block.shape}")
+
+
+
+
+# Create an instance of TransformerEncoderBlock
+transformer_encoder_block = TransformerEncoderBlock()
+
+# # Print an input and output summary of our Transformer Encoder (uncomment for full output)
+summary(model=transformer_encoder_block,
+        input_size=(1, 197, 768), # (batch_size, num_patches, embedding_dimension)
+        col_names=["input_size", "output_size", "num_params", "trainable"],
+        col_width=20,
+        row_settings=["var_names"])
+
+
+
+
+
+
+
+# Example of creating the class embedding and expanding over a batch dimension
+batch_size = 32
+class_token_embedding_single = nn.Parameter(data=torch.randn(1, 1, 768)) # create a single learnable class token
+class_token_embedding_expanded = class_token_embedding_single.expand(batch_size, -1, -1) # expand the single learnable class token across the batch dimension, "-1" means to "infer the dimension"
+
+# Print out the change in shapes
+print(f"Shape of class token embedding single: {class_token_embedding_single.shape}")
+print(f"Shape of class token embedding expanded: {class_token_embedding_expanded.shape}")
+
+
+
+
+
+
+
+
+
+
+set_seeds()
+
+# Create a random tensor with same shape as a single image
+random_image_tensor = torch.randn(1, 3, 224, 224) # (batch_size, color_channels, height, width)
+
+# Create an instance of ViT with the number of classes we're working with (pizza, steak, sushi)
+vit = ViT(num_classes=len(class_names))
+
+# Pass the random image tensor to our ViT instance
+print(vit(random_image_tensor))
+
+
+# Print a summary of our custom ViT model using torchinfo (uncomment for actual output)
+summary(model=vit,
+        input_size=(32, 3, 224, 224), # (batch_size, color_channels, height, width)
+        # col_names=["input_size"], # uncomment for smaller output
+        col_names=["input_size", "output_size", "num_params", "trainable"],
+        col_width=20,
+        row_settings=["var_names"]
+)
+
+
+
+
+# Setup the optimizer to optimize our ViT model parameters using hyperparameters from the ViT paper
+optimizer = torch.optim.Adam(params=vit.parameters(),
+                             lr=3e-3, # Base LR from Table 3 for ViT-* ImageNet-1k
+                             betas=(0.9, 0.999), # default values but also mentioned in ViT paper section 4.1 (Training & Fine-tuning)
+                             weight_decay=0.3) # from the ViT paper section 4.1 (Training & Fine-tuning) and Table 3 for ViT-* ImageNet-1k
+
+# Setup the loss function for multi-class classification
+loss_fn = torch.nn.CrossEntropyLoss()
+
+# Set the seeds
+set_seeds()
+
+# Train the model and save the training results to a dictionary
+results = engine.train(model=vit,
+                       train_dataloader=train_dataloader,
+                       test_dataloader=test_dataloader,
+                       optimizer=optimizer,
+                       loss_fn=loss_fn,
+                       epochs=10,
+                       device=device)
+
+
+
+from helper_functions import plot_loss_curves
+
+# Plot our ViT model's loss curves
+plot_loss_curves(results)
+
+plt.savefig("vit_loss_curves.png", dpi=300, bbox_inches='tight')
+plt.close()
